f42a0e87575943b05d4b83aeffe98db59cfa62c9
Sittipol Manitkul <workpapio@gmail.com>
2022-08-01 11:34:59 +0700

    setup authentications services

--- server/src/controllers/authentications.controller.js
+++ server/src/controllers/authentications.controller.js
@@ -1,10 +1,14 @@
+const { loginService, registerService } = require("../services/authentications.services/authentications.services");
+
 login = (req, res) => {
   console.log("login");
+  loginService(req.body);
   res.send("login");
 };
 
 register = (req, res) => {
   console.log("reigister");
+  registerService(req.body);
   res.send("register");
 };
 
---
+++ server/src/services/authentications.services/authentications.services.js
@@ -0,0 +1,9 @@
+loginService = (loginBody) => {
+  console.log("loginService");
+};
+
+registerService = (registerBody) => {
+  console.log("registerService");
+};
+
+module.exports = { loginService, registerService };
