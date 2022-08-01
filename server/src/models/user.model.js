3472917bf35bb2be1e81ed92cecced9d9f9bc088
Sittipol Manitkul <workpapio@gmail.com>
2022-08-01 12:35:54 +0700

    create user model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const connection = mongoose.createConnection(process.env.MONGODB_URL);
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  fname: String,
+  lname: String,
+  email: String,
+});
+
+const User = connection.model("User", userSchema);
+module.exports = { User };
